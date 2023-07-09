565889c49e9cec0dd3e3357974626d0d240fe76b
boxingnguyen <boxingnguyen@gmail.com>
2023-07-09 12:01:36 +0700

    create project

---
+++ bookXpand/bookXpandApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookXpandApp.swift
+//  bookXpand
+//
+//  Created by Boxing on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct bookXpandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
